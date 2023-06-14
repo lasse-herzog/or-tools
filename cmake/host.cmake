e96d72ae7f8b8a313584c8a5b88e13e01061a9ed
Lasse Herzog <lasse@herzoeglich.de>
2023-06-14 22:59:26 +0200

    Test if it compiles with native binary protoc

--- cmake/host.cmake
+++ cmake/host.cmake
@@ -11,6 +11,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+if ("${_cmake_compiler_output}" MATCHES "[Ee]mscripten")
+    set(PROTOC_PRG protoc)
+    return()
+endif ()
+
 if(NOT CMAKE_CROSSCOMPILING)
   set(PROTOC_PRG protobuf::protoc)
   return()
